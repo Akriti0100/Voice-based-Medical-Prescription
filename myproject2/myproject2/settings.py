6461463382ab10f7b9abf9912b95b97d80ca6190
Ayushjain1722 <42894689+Ayushjain1722@users.noreply.github.com>
2020-01-15 22:24:06 +0530

    Update settings.py

--- myproject2/myproject2/settings.py
+++ myproject2/myproject2/settings.py
@@ -124,5 +124,5 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'ayushjain1722@gmail.com'
-EMAIL_HOST_PASSWORD = 'Dragonballz1'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
