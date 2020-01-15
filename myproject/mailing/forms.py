b64e73a458d26caab7836a17bc11bf096533f3ec
Ayushjain1722 <42894689+Ayushjain1722@users.noreply.github.com>
2020-01-15 18:03:20 +0530

    First attempt for email attatchment

---
+++ myproject/mailing/forms.py
@@ -0,0 +1,13 @@
+from .models import Mails
+from django import forms
+
+
+class EmailForm(forms.ModelForm):
+    email = forms.EmailField(max_length=200, widget=forms.TextInput(attrs={'class': "form-control",'id': "clientemail"}))
+    message = forms.CharField( widget=forms.Textarea(attrs={'class': "form-control"}))
+    subject = forms.CharField( widget=forms.TextInput(attrs={'class': "form-control"}))
+
+
+class Meta:
+    model = Mails
+    fields = ('email', 'subject', 'message', 'document',)
